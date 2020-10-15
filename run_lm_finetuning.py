07b5e49634aba2052838181ac04051f4f2b5b5f6
TimofeyKorzh <tkorghebin@gmail.com>
2020-10-15 20:24:22 +0300

    Fixed logs

--- run_lm_finetuning.py
+++ run_lm_finetuning.py
@@ -330,7 +330,8 @@ def train(args, train_dataset, model, tokenizer):
     try:    
         for _ in train_iterator:
             epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
-            for step, batch in enumerate(epoch_iterator):
+            #for step, batch in enumerate(epoch_iterator):
+            for step, batch in enumerate(train_dataloader):
                 inputs, labels = mask_tokens(batch, tokenizer, args) if args.mlm else (batch, batch)
                 inputs = inputs.to(args.device)
                 labels = labels.to(args.device)
